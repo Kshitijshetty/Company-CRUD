70f6a6a68556cfdc9156b98f0677d2eab83d086e
Kshitijshetty <41435065+Kshitijshetty@users.noreply.github.com>
2021-08-24 09:10:09 +0530

    Delete Backend directory

--- Backend/db/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const dotenv=require('dotenv')
-
-dotenv.config()
-mongoose.connect(process.env.DB_ACCESS, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-},()=>{console.log('connected');})
